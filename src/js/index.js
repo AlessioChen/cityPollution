f84bb27853078a862b67ea0357c2c5bae2ad0496
alessio <alessiochen98@gmail.com>
2021-03-24 11:53:52 +0100

    - Aggiornato il layout. - Aggiornato funzioni JS con JQuery.

--- src/js/index.js
+++ src/js/index.js
@@ -4,33 +4,52 @@ import { APIRequest } from './utils'
 import { geoLocation } from './geoLocation'
 import $ from 'jquery'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import swal from 'sweetalert';
+
 
 //requestAQI(city);
 const token = process.env.AQI_TOKEN;
 const apiUrl = 'https://api.waqi.info/feed/';
-let timer = null;
-
 
 
 //API REQUEST WITH CITY  NAME 
-$('#input').keyup(function () {
-    let city = $("#input").val();
-    //TODO if city ='' handle it
+// $('#request').keyup(function () {
+//     let city = $("#input").val();
+//     //TODO if city ='' handle it
 
-    if (city != '') {
+//     if (city != '') {
+//         
+
+//         if (timer) clearTimeout(timer);
+//         timer = setTimeout(function () {
+//             APIRequest(url);
+//         }, 250);
+//     }
+
+// });
+
+$(function () {
+    $('#request').submit(function () {
+        let city = $("#city").val();
+
+        if (city == '') {
+            swal("Campo città vuoto", "...inserisci una città !");
+            return false;
+        }
+
         let url = apiUrl + city + '/?token=' + token;
 
-        if (timer) clearTimeout(timer);
-        timer = setTimeout(function () {
-            APIRequest(url);
-        }, 250);
-    }
+        APIRequest(url);
+
+        return false;
+
+
+    })
 
 });
 
 
-
-//API REQUEST when the page is load 
+// //API REQUEST with user coordinates when the page is load 
 $(document).ready(geoLocation());
 
 
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,49 +1,100 @@
 import $ from 'jquery'
+import swal from 'sweetalert';
+
+
 
 
 export function APIRequest(url) {
 
-    $.getJSON(url, function (result) {
-
-        console.log(result.data.time.s);
-
-        //TODO wrong input
-        if (!result || result.status != 'ok') {
-            console.log('Sorry, somenthin wrong happend: ');
-        }
-
-        //TODO no data
-        if (result.data.lenght == 0) {
-            console.log("Sorry, there is no result for you query!");
-        }
-
-        //city name & time 
-        let elem = document.getElementById('stationName');
-        if (result.data.city != null)
-            elem.textContent = "Stazione: " + result.data.city.name + " alle: " + result.data.time.s;
-
-
-
-        //aqi value 
-        elem = document.getElementById('staionValue');
-        elem.textContent = result.data.aqi;
-
-        for (let id in result.data.iaqi) {
-            let l = result.data.iaqi;
-            let value = l[id].v;
-            elem = document.getElementById(id);
-
-            if (elem != null)
-                elem.textContent = value;
-
-
-
-        }
+    $.ajax({
+        dataType: "json",
+        url: url,
+        data: "",
+        success: function () { console.log("API request completed") },
 
     });
+
+    $.getJSON(url, function (data) {
+
+        if (data.status != 'ok') {
+            swal("Citta' non valida!", "...inserisci una città valida!");
+            return false;
+        }
+
+        assign(data);
+    });
+
+
+
+
+
+    // $.getJSON(url, function (result) {
+
+    //     console.log(result.data.time.s);
+
+    //     //TODO wrong input
+    //     if (!result || result.status != 'ok') {
+    //         console.log('Sorry, somenthin wrong happend: ');
+    //     }
+
+    //     //TODO no data
+    //     if (result.data.lenght == 0) {
+    //         console.log("Sorry, there is no result for you query!");
+    //     }
+
+    //     //city name & time 
+    //     let elem = document.getElementById('stationName');
+    //     if (result.data.city != null)
+    //         elem.textContent = "Stazione: " + result.data.city.name + " alle: " + result.data.time.s;
+
+
+
+    //     //aqi value 
+    //     elem = document.getElementById('staionValue');
+    //     elem.textContent = result.data.aqi;
+
+    //     for (let id in result.data.iaqi) {
+    //         let l = result.data.iaqi;
+    //         let value = l[id].v;
+    //         elem = document.getElementById(id);
+
+    //         if (elem != null)
+    //             elem.textContent = value;
+
+
+
+    //     }
+
+    // });
+}
+function assign(data) {
+    $('#city-name').html(data.data.city.name);
+    $('#time-info').html(data.data.time.s);
+    $('#aqi-text').html( "AQI <span id='aqi'>"+ data.data.aqi+"</span> ");
+
+    colorize('aqi', data.data.aqi);
+
+    for (let id in data.data.iaqi) {
+        $('#' + id).html(data.data.iaqi[id].v);
+        colorize(id, data.data.iaqi[id].v);
+    }
+
+
 }
 
-function jsUcfirst(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
 
+
+function colorize(id, value) {
+    // console.log(id, value);
+
+    switch (true) {
+        case (value < 50): $('#' + id).addClass('good'); break;
+        case (value < 100): $('#' + id).addClass('normal'); break;
+        case (value < 150): $('#' + id).addClass('sensitive'); break;
+        case (value < 200): $('#' + id).addClass('bad'); break;
+        case (value < 300): $('#' + id).addClass('verybad'); break;
+          case (value > 300): $('#' + id).addClass('danger'); break;
+      
+    }
+
+}
