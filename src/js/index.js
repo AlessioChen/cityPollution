30f6c8ab929f2950e78adec31bc5bfbae9fd503e
alessio <alessiochen98@gmail.com>
2021-03-24 15:26:36 +0100

    -updated with footer

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,14 @@ import { geoLocation } from './geoLocation'
 import $ from 'jquery'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import swal from 'sweetalert';
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 
+library.add(fas, far, fab) 
+
+dom.i2svg() 
 
 //requestAQI(city);
 const token = process.env.AQI_TOKEN;
